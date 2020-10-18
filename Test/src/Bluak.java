3193d6bfe8889432b170bc50a11e2f40232d7629
Omkar Bangar <ombangar0@gmail.com>
2020-10-18 19:10:15 +0530

    input from file

--- Test/src/Bluak.java
+++ Test/src/Bluak.java
@@ -1,20 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Bluak {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Ok");
+	public static void main(String[] args) throws FileNotFoundException  {
 		
 		
+	Scanner input = new Scanner(new File("List.txt"));
+	
+	List<String> data = new ArrayList<String>();
+	
+	do {
+		data.add(input.nextLine());
 		
 		
+	}while (input.hasNextLine());
+	
+	
+	for(int i = 0 ; i < data.size() ; i++) {
 		
-		
-		
-		
-		
-		
-
+		System.out.println(data.get(i));
 	}
-
+	
+	
+		
+		
+	}
+	
+	
 }
